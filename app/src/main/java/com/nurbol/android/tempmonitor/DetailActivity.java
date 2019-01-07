9ee9112ff5986c6b7de3613548d412b4fc0a6347
Nurbol Kaldybayev <thenurbol@gmail.com>
2019-01-07 15:56:58 +0100

    swipe refresh to detail activity

--- app/src/main/java/com/nurbol/android/tempmonitor/DetailActivity.java
+++ app/src/main/java/com/nurbol/android/tempmonitor/DetailActivity.java
@@ -1,5 +1,7 @@
 package com.nurbol.android.tempmonitor;
 
+import android.os.Handler;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -20,23 +22,29 @@ import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
-public class DetailActivity extends AppCompatActivity {
+public class DetailActivity extends AppCompatActivity implements SwipeRefreshLayout.OnRefreshListener {
     TextView tempTextView, hmdtTextView, lightTextView, dateTextView, roomTextView;
     private RequestQueue mQueue;
     private static final String REQUEST_URL = "http://192.168.137.1:8080/api/sensors/data/";
     private String Url;
+    private SwipeRefreshLayout mSwipeLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
 
+        mSwipeLayout = (SwipeRefreshLayout) findViewById(R.id.refresh);
+        mSwipeLayout.setOnRefreshListener(this);
+        mSwipeLayout.setColorSchemeResources(
+                R.color.blue_swipe, R.color.green_swipe,
+                R.color.orange_swipe, R.color.red_swipe);
 
-        tempTextView = (TextView)findViewById(R.id.tempTextView);
-        hmdtTextView = (TextView)findViewById(R.id.hmdtTextView);
-        lightTextView = (TextView)findViewById(R.id.lightTextView);
-        dateTextView = (TextView)findViewById(R.id.dateTextView);
-        roomTextView = (TextView)findViewById(R.id.roomTextView);
+        tempTextView = (TextView) findViewById(R.id.tempTextView);
+        hmdtTextView = (TextView) findViewById(R.id.hmdtTextView);
+        lightTextView = (TextView) findViewById(R.id.lightTextView);
+        dateTextView = (TextView) findViewById(R.id.dateTextView);
+        roomTextView = (TextView) findViewById(R.id.roomTextView);
 
         String sensorId = getIntent().getStringExtra("id");
         String sensorRoom = getIntent().getStringExtra("room");
@@ -49,6 +57,19 @@ public class DetailActivity extends AppCompatActivity {
         jsonParse();
     }
 
+    @Override
+    public void onRefresh() {
+        Log.d("my_tag", "refresh");
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                jsonParse();
+
+                mSwipeLayout.setRefreshing(false);
+            }
+        }, 3000);
+    }
+
     private void jsonParse() {
 
         JsonArrayRequest request = new JsonArrayRequest(Request.Method.GET, Url, null,
@@ -57,17 +78,17 @@ public class DetailActivity extends AppCompatActivity {
                     public void onResponse(JSONArray response) {
 
                         try {
-                                JSONObject dataEntity = response.getJSONObject(0);
+                            JSONObject dataEntity = response.getJSONObject(0);
 
-                                String temp = dataEntity.getString("temp");
-                                String hmdt = dataEntity.getString("hmdt");
-                                String date = dataEntity.getString("date");
-                                String light = dataEntity.getString("light");
+                            String temp = dataEntity.getString("temp");
+                            String hmdt = dataEntity.getString("hmdt");
+                            String date = dataEntity.getString("date");
+                            String light = dataEntity.getString("light");
 
-                                tempTextView.setText(temp + "°C");
-                                hmdtTextView.setText(hmdt + "%");
-                                lightTextView.setText(light);
-                                dateTextView.setText(dateFormat(date));
+                            tempTextView.setText(temp + "°C");
+                            hmdtTextView.setText(hmdt + "%");
+                            lightTextView.setText(light);
+                            dateTextView.setText(dateFormat(date));
                         } catch (JSONException e) {
                             e.printStackTrace();
                         }
@@ -82,7 +103,7 @@ public class DetailActivity extends AppCompatActivity {
         mQueue.add(request);
     }
 
-    private String dateFormat (String dateTime){
+    private String dateFormat(String dateTime) {
         SimpleDateFormat fmt = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSZ");
         Date dateReal = null;
         try {
